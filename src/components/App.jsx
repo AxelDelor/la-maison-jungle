82ddb32b6ff78dd626ddfdfa542e66a9c3224512
axel delor <axeldelor@gmail.com>
2026-03-31 12:03:59 +0200

    map et affiochage conditionnel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import Banner from './Banner'
 import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 const App = () => {
-  return <>
-    <Banner />
-    <Cart />
-  </>
+    return (
+        <div>
+            <Banner />
+            {/* <Cart /> */}
+            <ShoppingList />
+        </div>
+    )
 }
 
 export default App
