863172b67c5b5a86754cc8e7bd48c87b473fab54
axel delor <axeldelor@gmail.com>
2026-04-01 14:36:39 +0200

    usetate ajouté sur le panier

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import Cart from './components/Cart'
 import ShoppingList from './components/ShoppingList'
 import mjlogo from './assets/mjlogo.png'
 import Footer from './components/Footer'
+import "./styles/Layout.css"
 
 const App = () => {
-    return (
-        <div>
-            <Banner>
-                <img src={mjlogo} alt='La maison jungle' className='lmj-logo' />
-                <h1 className='lmj-title'>La maison jungle</h1>
-            </Banner>
-            {/* <Cart /> */}
-            <ShoppingList />
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Banner>
+        <img src={mjlogo} alt='La maison jungle' className='lmj-logo' />
+        <h1 className='lmj-title'>La maison jungle</h1>
+      </Banner>
+      <div className='lmj-layout-inner'>
+        <Cart />
+        <ShoppingList />
+      </div>
+        <Footer />
+    </div>
+  )
 }
 
 export default App
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,23 +1,48 @@
 import "../styles/Cart.css"
+import { useState } from "react"
+
 
 const Cart = () => {
-  const prices = [
-    { name: 'Monstera', price: 8 },
-    { name: 'Lierre', price: 10 },
-    { name: 'Bouquet de fleurs', price: 15 },
-  ];
+  // const prices = [
+  //   { name: 'Monstera', price: 8 },
+  //   { name: 'Lierre', price: 10 },
+  //   { name: 'Bouquet de fleurs', price: 15 },
+  // ];
 
-  const total = prices.reduce((acc, plant) => acc + plant.price, 0)
+  // const total = prices.reduce((acc, plant) => acc + plant.price, 0)
 
-  return (
-    <div className="cart">
-      <span>Mon Panier :</span>
-      <ul>
-        {prices.map((plant) => (<li key={plant.name}> {plant.name} - {plant.price}€</li>))}
-      </ul>
-      <span>Total : {total}€</span>
+  // return (
+  //   <div className="cart">
+  //     <span>Mon Panier :</span>
+  //     <ul>
+  //       {prices.map((plant) => (<li key={plant.name}> {plant.name} - {plant.price}€</li>))}
+  //     </ul>
+  //     <span>Total : {total}€</span>
+  //   </div>
+  // );
+
+  const monsteraPrice = 8
+  const [cart, updateCart] = useState(0)
+  const [isOpen, setIsOpen] = useState(false)
+
+  return isOpen ? (
+    <div className="lmj-cart">
+      <button className="lmj-cart-toggle-button" onClick={() => setIsOpen(false)}>Fermer le panier</button>
+      <h2>Panier</h2>
+      <div>
+        Monstera : {monsteraPrice}€
+        <button onClick={() => updateCart(cart + 1)}>
+          Ajouter
+        </button>
+      </div>
+      <h3>Total : {monsteraPrice * cart}€</h3>
+      <button onClick={() => updateCart(0)}>Vider le panier</button>
     </div>
-  );
+  ) : (
+    <div className="lmj-cart closed">
+      <button className="lmj-cart-toggle-button" onClick={() => setIsOpen(true)}>Ouvrir le panier</button>
+    </div>
+  )
 };
 
 export default Cart
